8d9b37a3d92e67efbc431905d4a6ed0b1e238297
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-13 01:18:43 +0900

    TEST

--- Tab-3/50.js
+++ Tab-3/50.js
@@ -7,12 +7,12 @@ nums	        result
 [3,3,3,2,2,2]	2
 */
 
-function solution(nums) {
+function solution(nums) {   
     var answer = 0;
 
     // 가져갈 수 있는 포켓몬 수 구하기
     let myMonCnt = nums.length/2;
-
+=
     // 배열에 있는 포켓몬 종류의 수 구하기 : new Set() 속성을 통해 중복값 제거 후 배열로 묶음
     let arrMonCnt = [...new Set(nums)].length;
     
